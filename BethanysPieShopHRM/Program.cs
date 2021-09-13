7ee49ece97d3305102e36f4c6d1d512d1011bf85
Suyatna <zuyatna@gmail.com>
2021-09-13 19:34:33 +0700

    36 - Demo - Creating Methods

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -2,13 +2,29 @@
 
 namespace BethanysPieShopHRM
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Program
     {
         static void Main(string[] args)
         {
+            int monthlyWage = 1234;
+            int months = 12;
             
+            int yearlyWage = CalculateYearlyWage(monthlyWage, months);
+
+            Console.WriteLine($"Yearly wage (from main): {yearlyWage}");
             
             Console.ReadLine();
+        }
+
+        private static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
+        {
+            if (numberOfMonthsWorked == 12)
+            {
+                return monthlyWage * (numberOfMonthsWorked + 1);
+            }
+            
+            return monthlyWage * numberOfMonthsWorked;
         }
     }
 }
