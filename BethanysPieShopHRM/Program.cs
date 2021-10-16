e1e51c6750167290ac00237076608434d7a873e7
Suyatna <zuyatna@gmail.com>
2021-10-16 09:08:56 +0700

    adding HR folder

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -5,40 +5,9 @@ namespace BethanysPieShopHRM
     // ReSharper disable once ClassNeverInstantiated.Global
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int monthlyWage = 1234;
-            int months = 12;
-            
-            int yearlyWage = CalculateYearlyWage(monthlyWage, months);
-
-            Console.WriteLine($"Yearly wage (from main): {yearlyWage}");
-            
-            Console.ReadLine();
-        }
-
-        private static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked)
-        {
-            if (numberOfMonthsWorked == 12)
-            {
-                return monthlyWage * (numberOfMonthsWorked + 1);
-            }
-            
-            return monthlyWage * numberOfMonthsWorked;
-        }
-        
-        private static int CalculateYearlyWage(int monthlyWage, int numberOfMonthsWorked, int bonus)
-        {
-            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
-            
-            return monthlyWage * numberOfMonthsWorked + bonus;
-        }
-        
-        private static double CalculateYearlyWage(double monthlyWage, int numberOfMonthsWorked, double bonus)
-        {
-            Console.WriteLine($"The yearly wage is: {monthlyWage * numberOfMonthsWorked + bonus}");
-            
-            return monthlyWage * numberOfMonthsWorked + bonus;
+            Console.WriteLine("Hello, World");
         }
     }
 }
